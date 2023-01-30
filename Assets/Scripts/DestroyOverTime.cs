d36e4bfd5c20e62a540ed1d93d6ecd73e4314c9d
Vlad <vladekbur@gmail.com>
2023-01-30 13:32:58 +0200

    final

---
+++ Assets/Scripts/DestroyOverTime.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DestroyOverTime : MonoBehaviour
+{
+  [SerializeField] private float _timeToDestroy;
+
+  private void Start() => 
+    StartCoroutine("DestroyCoroutine");
+
+  IEnumerator DestroyCoroutine()
+  {
+    yield return new WaitForSeconds(_timeToDestroy);
+    Destroy(gameObject);
+  }
+}
